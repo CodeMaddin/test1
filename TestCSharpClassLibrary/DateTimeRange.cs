1e77e2a9c9553ebf6778b68dc466dc6789d3f277
CodeMaddin <41529568+CodeMaddin@users.noreply.github.com>
2024-08-24 23:20:31 -0500

    DateTimeRange got kleeened up

--- TestCSharpClassLibrary/DateTimeRange.cs
+++ TestCSharpClassLibrary/DateTimeRange.cs
@@ -2,65 +2,50 @@ using System.Text.Json.Serialization;
 
 namespace System
 {
-    public readonly struct DateTimeRange
+    public readonly struct DateTimeRange : IEquatable<DateTimeRange>
     {
         public static readonly DateTimeRange Anytime = new DateTimeRange(DateTime.MinValue, DateTime.MaxValue);
         public static readonly DateTimeRange Never = new DateTimeRange(DateTime.MinValue, DateTime.MinValue);
 
-        public DateTime Start { get;  }
+        public DateTime Start { get; }
         public DateTime End { get; }
         [JsonIgnore]
-        public TimeSpan Duration => End - Start; 
+        public TimeSpan Duration => End - Start;
 
-        public bool IsWithin(DateTimeRange range)
-            => (Start >= range.Start) && (End <= range.End);
-
-        public bool Intersects(DateTimeRange range)
-            => (Start <= range.End) && (End >= range.Start);
-
-        public bool Intersects(DateTime date)
-            => (Start <= date) && (End >= date);
-
+        public bool Contains(DateTime date) => (Start <= date) && (End >= date);
+        public bool Contains(DateTimeRange range) => (Start <= range.Start) && (End >= range.End);
+        public bool Intersects(DateTimeRange range) => (Start <= range.End) && (End >= range.Start);
+        
         public static IEnumerable<DateTimeRange> Union(ICollection<DateTimeRange> ranges)
         {
-            if (ranges.Count > 0)
+            if (ranges.Count == 0) yield break;
+
+            var sortedRanges = ranges.OrderBy(r => r.Start).ToList();
+            var current = sortedRanges[0];
+
+            foreach (var range in sortedRanges.Skip(1))
             {
-                ranges = ranges.OrderBy(r => r.Start).ToList();
-                var minStart = ranges.First().Start;
-                var maxFinish = ranges.First().End;
-
-                DateTime? lastFinish = null;
-
-                foreach (var r in ranges.Skip(1))
+                if (range.Start > current.End)
                 {
-                    if (r.Start > maxFinish)
-                    {
-                        lastFinish = maxFinish;
-                        yield return new DateTimeRange(minStart, maxFinish);
-
-                        minStart = r.Start;
-                        maxFinish = r.End;
-                    }
-                    else
-                    {
-                        if (r.End > maxFinish)
-                            maxFinish = r.End;
-                    }
+                    yield return current;
+                    current = range;
                 }
-
-                if(!lastFinish.HasValue || lastFinish != maxFinish)
-                    yield return new DateTimeRange(minStart, maxFinish);
+                else if (range.End > current.End)
+                {
+                    current = new DateTimeRange(current.Start, range.End);
+                }
             }
+
+            yield return current;
         }
 
         public IEnumerable<DateTimeRange> Inverse()
         {
-            // The opposite of all time is never...
-            if (Start == DateTime.MinValue && End == DateTime.MaxValue)
+            if (this == Anytime)
                 yield break;
 
             if (Start > DateTime.MinValue)
-                yield return new DateTimeRange(DateTime.MinValue, Start - DateTime.MinValue);
+                yield return new DateTimeRange(DateTime.MinValue, Start);
 
             if (End < DateTime.MaxValue)
                 yield return new DateTimeRange(End, DateTime.MaxValue);
@@ -68,39 +53,25 @@ namespace System
 
         public IEnumerable<DateTimeRange> Intersect(IEnumerable<DateTimeRange> ranges)
         {
-            foreach (var r in ranges)
-            {
-                var newRange = Intersect(r);
-                if(newRange != Never)
-                    yield return newRange;
-            }
+            return ranges.Select(Intersect).Where(r => r != Never);
         }
 
-        public DateTimeRange Intersect(DateTimeRange r)
+        public DateTimeRange Intersect(DateTimeRange range)
         {
-            var rangeStart = r.Start;
-            var rangeFinish = r.End;
-            if (rangeStart < Start)
-                rangeStart = Start;
+            var start = (range.Start < Start) ? Start : range.Start;
+            var end = (range.End > End) ? End : range.End;
 
-            if (rangeFinish > End)
-                rangeFinish = End;
-
-            if (rangeFinish >= Start && rangeStart <= End
-                && rangeStart != rangeFinish)
-                return new DateTimeRange(rangeStart, rangeFinish);
-            else
-                return Never;
+            return start < end ? new DateTimeRange(start, end) : Never;
         }
 
         public DateTimeRange(DateTime start, TimeSpan timeSpan) : this(start, start + timeSpan) { }
-        public DateTimeRange(DateTime start, DateTime finish)
+        public DateTimeRange(DateTime start, DateTime end)
         {
-            if (finish < start)
-                throw new ArgumentOutOfRangeException($"{nameof(finish)}", finish, "The finish date cannot be before the start date.");
+            if (end < start)
+                throw new ArgumentOutOfRangeException(nameof(end), end, "The end date cannot be before the start date.");
 
             Start = start;
-            End = finish;
+            End = end;
         }
 
         #region Overrides & Impliments
@@ -108,15 +79,19 @@ namespace System
         public string ToString(bool includeTime)
         {
             if (Never == this) return "Never";
-            else if (includeTime) return $"{Start} - {End} [{Duration}]";
-            else return $"{Start:d} - {End:d} [{Duration.TotalDays:0}]";
+            else if (Anytime == this) return "Anytime";
+
+            return includeTime
+                 ? $"{Start} - {End} [{Duration}]"
+                 : $"{Start:d} - {End:d} [{Duration.TotalDays:0}]";
         }
 
-        public override bool Equals(object? obj) => (obj is DateTimeRange range) && range == this;
-        public override int GetHashCode() => Start.GetHashCode() + End.GetHashCode();
+        public bool Equals(DateTimeRange other) => Start == other.Start && End == other.End;
+        public override bool Equals(object? obj) => (obj is DateTimeRange other) && Equals(other);
+        public override int GetHashCode() => HashCode.Combine(Start, End);
 
-        public static bool operator ==(DateTimeRange v1, DateTimeRange v2) => (v1.Start == v2.Start) && (v1.End == v2.End);
-        public static bool operator !=(DateTimeRange v1, DateTimeRange v2) => (v1.Start != v2.Start) || (v1.End != v2.End);
+        public static bool operator ==(DateTimeRange left, DateTimeRange right) => left.Equals(right);
+        public static bool operator !=(DateTimeRange left, DateTimeRange right) => !left.Equals(right);
         #endregion
     }
 }
